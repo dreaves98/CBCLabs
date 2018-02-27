98010a6a5c1b55720fd3610328a07bcd89ca4d32
dreaves98 <derrickreaves98@gmail.com>
2018-02-27 14:26:24 -0500

    finished Lab2

---
+++ navbar project/app/components/Home/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* NewHome
+*
+*/
+
+import React from 'react';
+
+import './style.css';
+import './styleM.css';
+
+export default class NewHome extends React.PureComponent {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
