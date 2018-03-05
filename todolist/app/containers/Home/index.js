bfe0927cf663b68bed79072de34fbbe5b33ec238
dreaves98 <derrickreaves98@gmail.com>
2018-03-05 15:24:38 -0500

    connected front and back end of to do list

--- todolist/app/containers/Home/index.js
+++ todolist/app/containers/Home/index.js
@@ -11,33 +11,13 @@ import './style.css';
 import './styleM.css';
 
 export default class Home extends React.PureComponent {
-  
-  render() {
-    return (
-       <div className="container">
-         <Helmet title="Home" meta={[ { name: 'description', content:   'Description of Home' }]}/>
-         
-          <div className="inputContainer">
-            <input type="text" className="todoInput" onChange={this.handleItem} value={this.state.inputItem} />
-            <input type="submit" value="Add to List" className="todoButton" onClick={this.storeItem} />
-          </div>
-          <div className="todoList">
-          {this.state.listItems.map((item, index) => (
-            <div className="listItem" key={index}>
-              {item}
-            </div>
-          ))}
-          </div>
-      </div>
-    );
-  }
   constructor() {
     super();
     this.state = {
       listItems:[],
       inputItem:""
     }
-  };  
+  };
   handleItem = (event) => {
     this.setState({
       inputItem:event.target.value
@@ -46,14 +26,70 @@ export default class Home extends React.PureComponent {
   storeItem = () => {
     var listItems = this.state.listItems;
     var inputItem = this.state.inputItem;
-    
+
     if(inputItem !=="") {
       listItems.push(inputItem);
-      
+
       this.setState({
         listItems:listItems,
         inputItem:""
       })
     }
   };
+  getTasks = () => {
+    fetch('http://localhost:8000/api/getTasks',{
+      method:'GET'
+    })
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json) {
+      this.setState({
+        listItems:json.tasks
+      })
+    }.bind(this))
+  };
+  storeTask = () => {
+    let data = new FormData();
+    data.append('taskContent', this.state.inputItem);
+
+   fetch('http://localhost:8000/api/storeTask', {
+     method:'POST',
+     body:data
+   })
+   .then(function(response){
+     return response.json();
+   })
+   .then(function(json) {
+     let listItems = this.state.listItems;
+     listItems.push(json.task);
+     this.setState({
+       listItems:listItems
+     })
+     this.forceUpdate();
+   }.bind(this))
+ };
+ componentWillMount() {
+   this.getTasks();
+ }
+  render() {
+    return (
+       <div className="container">
+         <Helmet title="Home" meta={[ { name: 'description', content:   'Description of Home' }]}/>
+
+          <div className="inputContainer">
+            <input type="text" className="todoInput" onChange={this.handleItem} value={this.state.inputItem} />
+            <input type="submit" value="Add to List" className="todoButton" onClick={this.storeTask} />
+          </div>
+          <div className="todoList">
+            {this.state.listItems.map((item, index) => (
+            <div className="listItem" key={index}>
+              {item.taskContent}
+            </div>
+          ))}
+          </div>
+      </div>
+    );
+  }
+
 }
